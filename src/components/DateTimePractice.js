28e1b8f701b72698ab7451b792d7c4b8a49e2108
Bryant Richards <dragonseyeimages@gmail.com>
2020-09-28 06:34:38 -0500

    setup basic, installed material ui icons

---
+++ src/components/DateTimePractice.js
@@ -0,0 +1,15 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { DateTime } from 'luxon'
+// material ui
+import PickerToolbar from '@material-ui/pickers/_shared/PickerToolbar'
+import ToolbarButton from '@material-ui/pickers/_shared/ToolbarButton'
+import { MuiPickersUtilsProvider, DatePicker } from '@material-ui/pickers'
+import { Container, Grid, IconButton, List, ListItem, ListItemText, ListItemAvatar, Avatar, Button, Box, DialogContentText, Dialog, DialogActions, DialogContent, DialogTitle, makeStyles } from '@material-ui/core'
+import CheckIcon from '@material-ui/icons/Check'
+import DateFnsUtils from '@date-io/date-fns'
+
+export default function DateTimeProactice () {
+  return (
+    <h1>Hello</h1>
+  )
+}
